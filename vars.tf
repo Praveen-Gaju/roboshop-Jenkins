1bcb033ac16d37f0a407ac301b0b115c57d8756b
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-17 12:29:09 +0530

    updating roboshop-Jenkins directory

--- vars.tf
+++ vars.tf
@@ -5,12 +5,12 @@ variable "folders" {
 variable "jobs" {
   default = [
     { folder = "Infra", name = "roboshop", repo_url = "https://github.com/Praveen-Gaju/roboshop-infra" },
-    { folder = "CI-Pipeline", name = "frontend", repo_url = "https://github.com/Praveen-Gaju/frontend.git" },
-    { folder = "CI-Pipeline", name = "catalogue", repo_url = "https://github.com/Praveen-Gaju/catalogue.git" },
-    { folder = "CI-Pipeline", name = "user", repo_url = "https://github.com/Praveen-Gaju/user.git" },
-    { folder = "CI-Pipeline", name = "cart", repo_url = "https://github.com/Praveen-Gaju/cart.git" },
-    { folder = "CI-Pipeline", name = "shipping", repo_url = "https://github.com/Praveen-Gaju/shipping.git" },
-    { folder = "CI-Pipeline", name = "payment", repo_url = "https://github.com/Praveen-Gaju/payment.git" },
-    { folder = "CI-Pipeline", name = "dispatch", repo_url = "https://github.com/Praveen-Gaju/dispatch.git" }
+    { folder = "CI-Pipeline", name = "frontend", repo_url = "https://github.com/Praveen-Gaju/frontend" },
+    { folder = "CI-Pipeline", name = "catalogue", repo_url = "https://github.com/Praveen-Gaju/catalogue" },
+    { folder = "CI-Pipeline", name = "user", repo_url = "https://github.com/Praveen-Gaju/user" },
+    { folder = "CI-Pipeline", name = "cart", repo_url = "https://github.com/Praveen-Gaju/cart" },
+    { folder = "CI-Pipeline", name = "shipping", repo_url = "https://github.com/Praveen-Gaju/shipping" },
+    { folder = "CI-Pipeline", name = "payment", repo_url = "https://github.com/Praveen-Gaju/payment" },
+    { folder = "CI-Pipeline", name = "dispatch", repo_url = "https://github.com/Praveen-Gaju/dispatch" }
   ]
 }
